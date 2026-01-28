cfa8e1b84088f1c8b1771398998c084609ed6d74
Edercitoflow <20253ds103@utez.edu.mx>
2026-01-27 20:11:51 -0600

    chore: base actividad git merge withoutconflict

---
+++ MergeWithOutConflict/src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Semana 2");
+        System.out.println("Versión base");
+    }
