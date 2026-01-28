9d116557008afb2eb5fa2d8c6481b704a6cf7834
Edercitoflow <20253ds103@utez.edu.mx>
2026-01-27 18:09:25 -0600

    se corrige el main de buenas practicas

--- ActividadBuenasPracticas/src/Main.java
+++ ActividadBuenasPracticas/src/Main.java
@@ -1,11 +1,26 @@
-
 import java.util.Scanner;
-public class Main{
-    public static void main(String[]a){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("n:");int n=sc.nextInt();
-        int s=0;
-        for(int i=1;i<=n;i++){s+=i;}
-        System.out.println("r:"+s);
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero = pedirEntero("Ingresa un número entero positivo: ");
+        int suma = sumarHastaN(numero);
+        System.out.println("La suma de los números del 1 al " + numero + " es: " + suma);
+    }
+
+    public static int pedirEntero(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        int valor = scanner.nextInt();
+        scanner.close();
+        return valor;
+    }
+
+    public static int sumarHastaN(int n) {
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+            suma += i;
+        }
+        return suma;
     }
 }
